8caf7320204efb1d68bcaa42de2a907419b43989
matthew.tomich <matthew.tomich@gmail.com>
2014-01-26 21:01:40 +0000

    date strings

--- src/main/java/com/matthewtomich/date/dateutil/App.java
+++ src/main/java/com/matthewtomich/date/dateutil/App.java
@@ -1,5 +1,9 @@
 package com.matthewtomich.date.dateutil;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Date date = new Date();
+
+        //  current date and time
+	System.out.println("Todays date: "+date.toString());
+        
+        // date formatting
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	System.out.println("Todays date: " + simpleDateFormat.format(date));
+        
+        SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss");
+	System.out.println("Todays date: " + simpleDateFormat2.format(date));
+        
+        
+        SimpleDateFormat simpleDateFormat3 = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+        // convert string
+	String strdate = "26-Jan-2014 09:00:40";
+	try {
+            Date newdate = simpleDateFormat2.parse(strdate);
+            System.out.println(newdate);
+	} catch (ParseException e) {
+            e.printStackTrace();
+	}
+		
     }
 }
