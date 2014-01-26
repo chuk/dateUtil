330698fdc93e678314566da0c82125047c6b029e
matthew.tomich <matthew.tomich@gmail.com>
2014-01-26 20:46:11 +0000

    first commit

---
+++ src/main/java/com/matthewtomich/date/dateutil/App.java
@@ -0,0 +1,13 @@
+package com.matthewtomich.date.dateutil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
